693e38d4afc4211bfb64dc1574b934389d09611b
Andrew Kim <66102642+ahkim3@users.noreply.github.com>
2021-04-14 00:57:55 -0500

    Add project files.

---
+++ Poly/Poly.cpp
@@ -0,0 +1,38 @@
+/*
+Name: Andrew Kim
+Date: April 14, 2021
+Program: Poly Lab
+
+I hereby certify that this program represents my
+work and that the design and logic was completed
+without outside assistance.
+
+Signature: Andrew Kim
+*/
+
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
+    sf::CircleShape shape(200.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
